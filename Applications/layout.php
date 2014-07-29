cc3dcf40a5e3139a002836dcdb29de69123af1f2
yohann-teisseire <yohan.teisseire@gmail.com>
2014-07-29 10:53:12 +0200

    first commit, new architecture

---
+++ Applications/layout.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title>Application Web</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+	<?php echo $content; ?>
+
+</body>
+</html>
